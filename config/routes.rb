af6fe7f2f430fb8331b0c60efe533dfd5d71df9a
waywho <walzerfan@yahoo.com>
2020-07-21 17:42:13 +0200

    add after sign_in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :chatrooms
   devise_for :users
+  get '/users' => 'chatrooms#index', as: :user_root
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
