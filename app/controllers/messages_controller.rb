f2100212cee018ad818857f256a120b105e7e846
waywho <walzerfan@yahoo.com>
2020-07-22 17:56:54 +0200

    adding redis configuration for message relay jobs

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,15 @@ class MessagesController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		message = current_user.messages.create(message_params)
+		message = current_user.messages.new(message_params)
+		if message.save
+		else
+		end
 	end
 
 
 	private
-	
+
 	def message_params
      params.require(:message).permit(:content, :chatroom_id)
  	end
